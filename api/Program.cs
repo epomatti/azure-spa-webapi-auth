b81223d8d4e087968ce4589a7d2dd48b992487d2
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-03-24 17:49:58 -0300

    startup

--- api/Program.cs
+++ api/Program.cs
@@ -1,33 +1,41 @@
 using Microsoft.Identity.Web;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using dotenv.net;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Logging;
 
 DotEnv.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-            .AddMicrosoftIdentityWebApp(options => builder.Configuration.Bind("AzureAd", options));
+// builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+//             .AddMicrosoftIdentityWebApi(options => builder.Configuration.Bind("AzureAd", options));
+
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration);
 
 builder.Services.AddHealthChecks();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Only for development
+IdentityModelEventSource.ShowPII = true;
+
 var app = builder.Build();
 
-app.MapHealthChecks("/healthz");
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
   app.UseSwaggerUI();
+  app.UseDeveloperExceptionPage();
 }
 
+app.UseRouting();
 app.UseAuthorization();
 app.UseAuthorization();
+app.MapHealthChecks("/healthz");
 
 app.MapControllers();
 
